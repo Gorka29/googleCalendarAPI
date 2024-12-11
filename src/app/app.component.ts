57830223f2b60cc7783b9cd2c16b47133647cfd0
Gorka29 <gorkaruizaraujo@gmail.com>
2024-12-11 16:09:06 +0100

    quitado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NewAppointmentComponent } from './new-appointment/new-appointment.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewAppointmentComponent],
+  imports: [NewAppointmentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
